c6bebbedc3287377b429353200c1bb11f8a0bd30
Samuel Janas <sneakyinsect@gmail.com>
2023-06-06 18:01:24 +0200

    Updated main

--- main.py
+++ main.py
@@ -8,21 +8,17 @@ pygame.init()
 
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
-screen.fill(Color.BLACK)
-pygame.draw.line(screen, Color.WHITE, (50, 500), (750, 500), 1)
-pygame.draw.circle(screen, Color.WHITE, (50, 500), 2)
-pygame.draw.circle(screen, Color.WHITE, (750, 500), 2)
+
 center_x, center_y = (50+750)/2, 500
-pygame.draw.circle(screen, Color.WHITE, (center_x, center_y), 2)
 
 # Title and Icon
 pygame.display.set_caption("Polyrhythm")
 # icon = pygame.image.load('assets/icon.png')
 
 number_of_notes = 11
-circle_width = 4
+circle_width = 6
 half_length = (750-50)/2
-circle_interval = (half_length-number_of_notes*circle_width)/number_of_notes+1
+circle_interval = (half_length)/(number_of_notes+1)
 
 
 # circle arc pairs: (circle, arc)
@@ -37,19 +33,31 @@ for i in range(number_of_notes):
 
 circle_arcs = zip(circles, arcs)
 
-# Game Loop
+for circle, arc in circle_arcs:
+    arc.draw()
+    circle.draw()
+
+clock = pygame.time.Clock()
 running = True
 while running:
     # RGB = Red, Green, Blue
-
+    clock.tick(12)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill(Color.BLACK)
+    pygame.draw.line(screen, Color.WHITE, (50, 500), (750, 500), 1)
+    pygame.draw.circle(screen, Color.WHITE, (50, 500), 2)
+    pygame.draw.circle(screen, Color.WHITE, (750, 500), 2)
+    pygame.draw.circle(screen, Color.WHITE, (center_x, center_y), 2)   
+
     for circle, arc in circle_arcs:
+        circle.move(arc.start_angle, arc.end_angle)
         arc.draw()
         circle.draw()
-        
-
 
     pygame.display.update()
+
+pygame.quit()
